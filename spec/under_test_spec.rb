bf984a8168704553ed4c2263b13e957e10492357
Adam Stegman <astegman@gmail.com>
2014-02-11 07:38:13 -0800

    Remove unnecessary file and indirection

--- spec/under_test_spec.rb
+++ spec/under_test_spec.rb
@@ -3,13 +3,12 @@ require_relative '../under_test'
 
 describe UnderTest do
   describe '.some_method' do
-    let!(:stubbed_class) { class_double("Stubbed").as_stubbed_const }
-
     subject { described_class.some_method }
 
     before do
+      class_double("Stubbed").as_stubbed_const
       class_double("Stubbed::SomeError").as_stubbed_const
-      allow(stubbed_class).to receive(:some_method).and_raise(Stubbed::SomeError)
+      allow(Stubbed).to receive(:some_method).and_raise(Stubbed::SomeError)
     end
 
     it "raises the error" do
--- stubbed.rb
+++
@@ -1,7 +0,0 @@
-module Stubbed
-  SomeError = Class.new(StandardError)
-
-  def some_method
-    false
-  end
-end
